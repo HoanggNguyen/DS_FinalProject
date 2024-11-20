c3d5967d20dd8146dbe6527c5fcc733a82306db4
nhohien <ngvanhien711@gmail.com>
2024-11-20 22:18:30 +0700

    fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -19,11 +19,12 @@ def furniture_to_number(furniture, dict_furniture):
 
 raw_df = pd.read_csv('./Data/DSPhongTro.csv', sep='\t')
 
-raw_df = raw_df.dropna(subset=['ID', 'Mức giá']) #Bỏ những dòng ko có ID và mức giá
-#Điền missing value của cột 'Số phòng ngủ', 'Số toilet' = 1.0 vì mỗi phòng trọ đều có tối thiểu 1 phòng ngủ, 1 toilet
-raw_df = raw_df.fillna(1.0)
+raw_df = raw_df.dropna(subset=['ID', 'Địa chỉ', 'Diện tích', 'Mức giá', 'Ngày đăng tin', 'Số phòng ngủ', 'Số toilet'])
 
-raw_df = raw_df[raw_df['Mức giá'] != 'Thỏa thuận'] #bỏ những dòng nào có giá là 'thỏa thuận'
+#Loại bỏ dòng trùng lắp
+raw_df.drop_duplicates(inplace=True)
+
+raw_df = raw_df[raw_df['Mức giá'] != 'Thỏa thuận'].reset_index(drop=True) #bỏ những dòng nào có giá là 'thỏa thuận'
 
 #Chuẩn hóa cột địa chỉ và nội thất
 districts = pd.read_csv('./Data/address.txt', sep=':', header=None)
@@ -39,6 +40,10 @@ raw_df['Mức giá'] = raw_df['Mức giá'].astype(np.float64)
 raw_df['Ngày đăng tin'] = pd.to_datetime(raw_df['Ngày đăng tin'], format='%d/%m/%Y')
 raw_df['Địa chỉ'] = raw_df['Địa chỉ'].astype('category')
 raw_df['Nội thất'] = raw_df['Nội thất'].astype('category')
+
+start_date = pd.to_datetime('01/10/2024', format='%d/%m/%Y')
+end_date = pd.to_datetime('31/10/2024', format='%d/%m/%Y')
+raw_df = raw_df[(raw_df['Ngày đăng tin'] >= '1/10/2024') & (raw_df['Ngày đăng tin'] <= '31/10/2024')]
 
 #Lưu dữ liệu đã xử lý vào file csv
 raw_df.to_csv('./Data/DSPhongTro_DaXuLy.csv', encoding='utf-8-sig', header=True, sep='\t')
